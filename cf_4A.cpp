9164e611a6ecaa498081e117887132359e6c4c10
Jahid Hasan Jony <66401188+jony-coder@users.noreply.github.com>
2021-03-19 19:32:05 +0600

    Add files via upload

---
+++ cf_4A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n, ordhek;
+    cin>>n;
+
+    if(n==2){
+        cout<<"NO"<<endl;
+    }
+
+    else if(n%2==0){
+            cout<<"YES"<<endl;
+    }
+
+    else{
+        cout<<"NO"<<endl;
+    }
+
+return 0;
+}
